e8ffca4348c543630f0aa6b96c81a5d85dd1d4f2
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2023-10-14 13:39:39 +0600

    Create label Attribute

---
+++ app/Attributes/Label.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Attributes;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_CLASS_CONSTANT)]
+class Label extends AttributeProperty {}
--- app/Enums/RoleEnum.php
+++ app/Enums/RoleEnum.php
@@ -4,6 +4,7 @@ namespace App\Enums;
 
 use App\Attributes\BackgroundColor;
 use App\Attributes\Description;
+use App\Attributes\Label;
 use App\Traits\AttributableEnum;
 
 enum RoleEnum: string
@@ -11,14 +12,17 @@ enum RoleEnum: string
      /* We use the trait we have created */
     use AttributableEnum;
 
+    #[Label('Administrator')]
     #[Description('He\'s got full powers')]
     #[BackgroundColor('red')]
     case ADMIN = 'admin';
 
+    #[Label('User')]
     #[Description('A classic user, with classic rights')]
     #[BackgroundColor('blue')]
     case USER = 'user';
 
+    #[Label('Guest')]
     #[Description('Oh, a guest, be nice to him!')]
     #[BackgroundColor('green')]
     case GUEST = 'guest';
