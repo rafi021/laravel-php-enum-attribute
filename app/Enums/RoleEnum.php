2899e09f8cfd4cc8c182fa417190092c1e5eddf4
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2023-10-14 12:42:14 +0600

    creating enums for role

---
+++ app/Enums/RoleEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+
+enum RoleEnum: string
+{
+    case ADMIN = 'admin';
+    case USER = 'user';
+    case GUEST = 'guest';
+}
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -2,9 +2,12 @@
     <h1 class="text-3xl font-bold">Users</h1>
      <ul>
          @foreach ($users as $user)
-             <li class="px-4 py-2 my-4 bg-white border border-gray-300 rounded">
-                 <p class="font-semibold">{{ $user->name }}</p>
-                 <p class=text-gray-500>{{ $user->email }}</p>
+             <li class="flex justify-between px-4 py-2 my-4 bg-white border border-gray-300 rounded">
+                 <div>
+                     <p class="font-semibold">{{ $user->name }}</p>
+                     <p class=text-gray-500>{{ $user->email }}</p>
+                 </div>
+                 <p class="font-bold text-gray-500">{{ $user->role }}</p>
              </li>
          @endforeach
      </ul>
